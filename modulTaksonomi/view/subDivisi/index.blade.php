f4029e54a239ae116708220e2ca189a22bf16172
Agus Darmawan <78064713+agus041mind@users.noreply.github.com>
2021-06-02 10:01:10 +0700

    Add files via upload

---
+++ modulTaksonomi/view/subDivisi/index.blade.php
@@ -0,0 +1,80 @@
+<!-- Menghubungkan dengan view adMaster -->
+@extends('masterPages.adMasterBS4')
+ 
+@section('konten')
+
+    <!-- Begin Main Content -->
+    <div class="container-fluid">
+
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Sub Divisi</h1>
+        </div>
+
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              @if(Session::has('sessPesanSindata'))
+              <div class="alert alert-success alert-dismissible">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <i class="fas fa-check-circle"></i> {{Session::get('sessPesanSindata')}}
+              </div>
+              @endif
+              <h6 class="m-0 font-weight-bold text-primary">
+              <a href="{{route('adminSubDivisiTambah')}}">
+						    <button type="button" class="btn btn-sm btn-primary" >
+						    <i class='fas fa-plus'></i> Tambah
+						    </button>
+					    </a>
+              </h6>
+            </div>
+            <div class="card-body" style="color:#000000">
+              <div class="table-responsive-xl">
+                <table class="table table-bordered table-striped table-sm" id="tableSindata" width="100%" cellspacing="0" style="color:#000000">
+                  <thead class="text-center bg-dark text-light">
+                    <tr>
+                      <th>Sub Divisi</th>
+                      <th>Divisi</th>
+                      <th></th>
+                      <th></th>
+                    </tr>
+                  </thead>
+                  <tfoot class="text-center bg-dark text-light">
+                    <tr>
+                      <th>Sub Divisi</th>
+                      <th>Divisi</th>
+                      <th></th>
+                      <th></th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    @foreach($subDivisi as $item)
+                    <tr>
+                      <td>{{$item->nama}}</td>
+                      <td>{{$item->divisi->nama}}</td>
+                      <td class="text-center">
+                        <a href="{{route('adminSubDivisiTampil',$item->idSubDivisi)}}">
+						              <button type="button" class="btn btn-sm btn-warning" >
+						              <i class='fas fa-edit'></i> Ubah
+						              </button>
+					              </a>
+                      </td>
+                      <td class="text-center">
+                        <a href="{{route('adminSubDivisiHapus',$item->idSubDivisi)}}">
+						              <button type="button" class="btn btn-sm btn-danger" onClick="return confirm('Yakin mau dihapus?')" 
+                          {{(($item->DetailPenerimaan == null || $item->DetailPenerimaan == 0) && ($item->UbahNama == null || $item->UbahNama == 0) && ($item->HerbariumJenis == null || $item->HerbariumJenis == 0)) ? "" : "disabled"}}
+                          >
+						              <i class='fas fa-trash'></i> Hapus
+						              </button>
+					              </a>
+                      </td>
+                    </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+            </div>
+        </div>
+          
+    </div>
+    <!-- End Main Content -->
+
+@endsection
---
+++ modulTaksonomi/view/subDivisi/tampil.blade.php
@@ -0,0 +1,61 @@
+<!-- Menghubungkan dengan view adMaster -->
+@extends('masterPages.adMasterBS4')
+ 
+@section('konten')
+
+    <!-- Begin Main Content -->
+    <div class="container-fluid">
+
+        <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">Sub Divisi</h1>
+        </div>
+
+          <div class="card shadow mb-4">
+            <div class="card-header py-3">
+              <h6 class="m-0 font-weight-bold text-primary">
+              Ubah Sub Divisi
+              </h6>
+            </div>
+            <div class="card-body py-3">
+
+            @if($errors->any())
+              <div class="alert alert-danger" role="alert">
+                <ul>
+                  @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                  @endforeach
+                </ul>
+              </div>
+            @endif
+
+            <form action="{{route('adminSubDivisiUbah',$subDivisi->idSubDivisi)}}" method="post">
+            @csrf
+              <!-- Hidden -->
+              <div class="form-group">
+                <input class="form-control" type="hidden" placeholder="" name="subDivisiHidden" value="{{$subDivisi->nama}}">
+              </div>
+              <!-- Hidden -->
+              <div class="form-group">
+                <label for="divisi">Divisi</label>
+                <select class="form-control select2Sindata" name="divisi">
+                  @foreach($divisi as $item)
+                  <option value="{{$item->idDivisi}}" {{$subDivisi->idDivisi == $item->idDivisi ? "selected=''" : ""}}>{{$item->nama}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group">
+                <label for="subDivisi">Sub Divisi</label>
+                <input class="form-control" type="text" placeholder="" name="subDivisi" value="{{$subDivisi->nama}}">
+              </div>
+              <button type="submit" class="btn btn-primary">Simpan</button>
+              <button type="button" class="btn btn-primary" onClick="window.location.reload()">Reset</button>
+              <button type="button" class="btn btn-primary" onClick="window.location.href='{{route('adminSubDivisi')}}'">Batal</button>
+            </form>            
+
+            </div>
+          </div>
+          
+    </div>
+    <!-- End Main Content -->
+
+@endsection
